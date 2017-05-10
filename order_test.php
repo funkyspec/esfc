c944644cffc167124dae341732866decc4b66323
Ric Gresia <mrgresia@gmail.com>
2017-05-10 16:23:13 -0400

    Make order_test page slightly more presentable

--- order_test.php
+++ order_test.php
@@ -1,3 +1,30 @@
 <?php
     echo $_GET['email'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>ESFC Order Page</title>
+
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="icon" href="images/favicon.png">
+
+</head>
+
+<body>
+
+  <h2>ESFC Order Page</h2>
+
+<p>Order page for <strong><?php echo $_GET['email']; ?></strong></p>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+
+</html>
